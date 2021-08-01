211b7f48d0c92b4186c82772e04e0cfc791eaf18
GithungoIan <githungoian53@gmail.com>
2021-08-01 12:08:32 +0300

    created email.js

---
+++ utils/email.js
@@ -0,0 +1,8 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+  // 1) Create transport
+  // 2) Define the mail options
+  // 3) Actually sed the mail
+};
+module.exports = sendEmail;
