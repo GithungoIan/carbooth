8f45dc2748d36cdf23a6f2537dc37fb79eb5195d
GithungoIan <githungoian53@gmail.com>
2021-08-01 17:13:02 +0300

    update user model

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
-  fistName: {
+  firstName: {
     type: String,
     required: [true, "Please provide your first name"],
   },
