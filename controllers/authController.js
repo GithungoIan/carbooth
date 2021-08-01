2303b7229fed742887c87962258ec3b0e523475d
GithungoIan <githungoian53@gmail.com>
2021-08-01 13:20:37 +0300

    feat: protect user routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,8 @@ const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
-const crypo = require("crypo");
+const crypto = require("crypto");
+const { promisify } = require("util");
 
 //  TODO:  signup  || register
 exports.signup = catchAsync(async (req, res, next) => {
@@ -180,5 +181,62 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 //  TODO: protect routes
-//  TODO: check if user is logged in for rendered pages only
+exports.protect = catchAsync(async (req, res, next) => {
+  // get the topken and check if it exist
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
+
+  if (!token) {
+    return next(
+      new AppError("You are not logged in, please login to gain access")
+    );
+  }
+
+  // verify the token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // check if the user still exists
+  const currentUser = await User.findById(decoded.id);
+
+  if (!currentUser) {
+    return next(
+      new AppError("User belonging to this token does not exist", 401)
+    );
+  }
+
+  // check if user changed password after token was issued
+  if (currentUser.changedPasswordsAfter(decoded.iat)) {
+    return next(
+      new AppError(
+        "You recently changed your password! please login again",
+        401
+      )
+    );
+  }
+
+  // grant access to the protected route
+  req.user = currentUser;
+  req.local.user = currentUser;
+  return next();
+});
+
 //  TODO: restrict to admin ||  users
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to perform this action")
+      );
+      next();
+    }
+  };
+};
+
+//  TODO: check if user is logged in for rendered pages only
