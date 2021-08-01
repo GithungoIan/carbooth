8bfdc1270c1386d309314bbbad406baf72508712
GithungoIan <githungoian53@gmail.com>
2021-08-01 12:37:36 +0300

    feat: implemented login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 //  TODO:  signup  || register
 exports.signup = catchAsync(async (req, res, next) => {
@@ -20,7 +21,60 @@ exports.signup = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+// sign the token
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+// create token for user
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    cookieOptions.secure = true;
+  }
+
+  // for browsers
+  res.cookie("jwt", token, cookieOptions);
+
+  // remove password from the Output
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
 //  TODO: login
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  // check if email and password exist
+  if (!email || !password) {
+    return next(new AppError("Please provide email and password", 400));
+  }
+
+  // check if user exist and password is correct
+  const user = await User.findOne({ email }).select("+password");
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError("Incorrect email or password", 401));
+  }
+
+  // if okay send token to the user
+  createSendToken(user, 200, res);
+});
 //  TODO: logout
 //  TODO: forgot password
 //  TODO: reset password
